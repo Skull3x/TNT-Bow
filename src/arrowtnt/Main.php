bfc5daa24577853e7d1d9f1ae07a80fc249dcdd6
Cøsʍic Sαѵαɢe <jross3740@gmail.com>
2016-08-20 12:14:36 -0400

    Update Main.php

--- src/arrowtnt/Main.php
+++ src/arrowtnt/Main.php
@@ -27,27 +27,29 @@ class Main extends PluginBase implements Listener{
             $z = $arrow->getZ();
             $level = $arrow->getLevel();
             $chunk = $level->getChunk(round($x) >> 4, round($z) >> 4);
-            if($player->getInventory()->getContents() as $item){
-                if($item->getId() == 46){
-                    $player->getInventory()->removeItem(Item::get(46, 0, 1));
-                    $tnt = Entity::createEntity("PrimedTNT", $chunk, new CompoundTag("", [
-                        "Pos" => new ListTag("Pos", [
-                            new DoubleTag("", $x),
-                            new DoubleTag("", $y),
-                            new DoubleTag("", $z)
-                            ]),//listtag pos
-                            "Motion" => new ListTag("Motion", [
-                                new DoubleTag("", 0),
-                                new DoubleTag("", 0),
-                                new DoubleTag("", 0)
-                                ]), //listag motion
-                                "Rotation" => new ListTag("Rotation", [
-                                    new FloatTag("", lcg_value() * 360),
-                                    new FloatTag("", 0)
-                                    ]), //listag rotation
-                                    ])     //WHOLE COMPOUND TAG
-                                    );//create entity
-                                    $tnt->spawnToAll();
+            if ($player->hasPermission("tnt.bow.use")){
+                if ($player->getInventory()->getContents() as $item){
+                    if ($item->getId() == 46){
+                        $player->getInventory()->removeItem(Item::get(46, 0, 1));
+                        $tnt = Entity::createEntity("PrimedTNT", $chunk, new CompoundTag("", [
+                            "Pos" => new ListTag("Pos", [
+                                new DoubleTag("", $x),
+                                new DoubleTag("", $y),
+                                new DoubleTag("", $z)
+                                ]),//listtag pos
+                                "Motion" => new ListTag("Motion", [
+                                    new DoubleTag("", 0),
+                                    new DoubleTag("", 0),
+                                    new DoubleTag("", 0)
+                                    ]), //listag motion
+                                    "Rotation" => new ListTag("Rotation", [
+                                        new FloatTag("", lcg_value() * 360),
+                                        new FloatTag("", 0)
+                                        ]), //listag rotation
+                                        ])     //WHOLE COMPOUND TAG
+                                        );//create entity
+                                        $tnt->spawnToAll();
+                    }
                 }
             }
         }
